a1d0a224ca1867a3a3bf5ef8968af05830587f44
AylinvanRossum <318854@student.saxion.nl>
2014-09-02 22:05:48 +0200

    Deleted Webtech1. You can now login. The users are added in Login.java.

--- Test/src/Main/Login.java
+++ Test/src/Main/Login.java
@@ -1,6 +1,8 @@
 package Main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,18 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class login
+ * Servlet implementation class Servlet1
  */
-@WebServlet("/login")
+@WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ArrayList<User> users = new ArrayList<User>();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public Login() {
 		super();
-		// TODO Auto-generated constructor stub
+		users.add(new User("Damon", "Daal"));
+		users.add(new User("Aylin", "vRos"));
 	}
 
 	/**
@@ -28,6 +32,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		request.getParameter("username");
 		getServletContext().getRequestDispatcher("/login.html").forward(
 				request, response);
 	}
@@ -38,7 +43,44 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("test");
+		if (isValidEntry(request)) {
+			getServletContext().getRequestDispatcher("/home.html").forward(
+					request, response);
+		} else {
+			getServletContext().getRequestDispatcher("/login.html").forward(
+					request, response);
+		}
+
+		// if(name.equals("register")) {
+		// getServletContext().getRequestDispatcher("/register.html").forward(request,
+		// response);
+		// } else if (name.equals("submit")) {
+		// System.out.println("niet registreren");
+		// }
+
+	}
+
+	private boolean isValidEntry(HttpServletRequest request) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (username == null && password == null) {
+			System.out.println("No entry given");
+		} else if (username == null && password != null) {
+			System.out.println("Please enter your username!");
+		} else if (username != null && password == null) {
+			System.out.println("Please enter your password!");
+		}
+		for (User u : users) {
+			if (username.equals(u.getUsername())) {
+				if (u.isValidPassword(password)) {
+					return true;
+				}
+				System.out.println("No valid password entered");
+				return false;
+			}
+		}
+		System.out.println("No valid user entered");
+		return false;
 	}
 
 }
